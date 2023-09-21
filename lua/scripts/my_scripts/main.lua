9c1fb2a57e7ef6b840466f2c67b8f6065502d483
Abdel-Waheb <103281009+L4KK4S@users.noreply.github.com>
2023-09-21 21:13:16 +0200

    Add exercice

--- lua/scripts/my_scripts/main.lua
+++ lua/scripts/my_scripts/main.lua
@@ -1,16 +1,38 @@
--- On charge l'image dans la VRAM
-image = Image.load("slime.png", VRAM)
-
-while not Keys.held.Start do
-
-    screen.drawGradientRect(SCREEN_UP, 0, 0, 256, 192, Color.new(31, 0, 0), Color.new(0, 31, 0), Color.new(31, 0, 0), Color.new(0, 31, 0))
+-- Exercice consistant à afficher deux rectangles avec l'un ayant une transparence opposée à l'autre
+-- La transparence est affichée puis gérée avec les boutons haut et bas
 
 
-    -- On affiche l'image en haut à gauche de l'écran tactile
-    screen.blit(SCREEN_DOWN, 0, 0, image) -- Affiche l'image
-    render()
-end
+coefficient = 50  -- initialisation du coefficient
 
--- Et on efface l'image de la mémoire
-Image.destroy(image)
-image = nil
+while not Keys.newPress.Start do   -- boucle du programme
+
+    Controls.read()  -- initialisation de la récupération des touches
+
+    if Keys.held.Up and coefficient < 99 then  -- cas touche haut
+        coefficient = coefficient + 1
+    end
+
+    if Keys.held.Down and coefficient > 0 then  -- cas touche bas
+        coefficient = coefficient - 1
+    end
+
+    -- affichage premier rect
+    tmp = coefficient 
+    screen.setAlpha(tmp)
+    screen.drawFillRect(SCREEN_DOWN, 0, 0, 50, 50, Color.new(31, 0, 0))
+
+    -- affichage deuxième rect
+    screen.setAlpha(99 - tmp)
+    screen.drawFillRect(SCREEN_DOWN, 110, 0, 50, 50, Color.new(0, 31, 0))
+
+    -- affichage texte
+    screen.setAlpha(99)
+    screen.print(SCREEN_UP, 0, 0, "Le coefficient est de:"..coefficient)
+
+    screen.setAlpha(coefficient)    -- ajustement coeff
+
+    render()    -- affichage 
+
+end  -- fin du programme
+
+coefficient = nil  -- libération de la mémoire
