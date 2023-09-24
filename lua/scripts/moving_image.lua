3f41ac775ec97e937cf71549050eb020385b18e7
Lakkas <abdel-waheb.sakkal@efrei.net>
2023-09-24 02:20:03 +0200

    optimize the variables(use Image.width(image))

--- lua/scripts/moving_image.lua
+++ lua/scripts/moving_image.lua
@@ -7,7 +7,7 @@ screen.blit(SCREEN_DOWN, 0, 0, background) -- display the background
 -- image specification
 x, y = 0, 0 
 slime = Image.load("data/img/slime.png", VRAM) -- load the slime image
-image_size = 32
+image_size = Image.width
 
 while not Keys.newPress.Start do   -- boucle du programme
 
@@ -21,14 +21,14 @@ while not Keys.newPress.Start do   -- boucle du programme
     y = y + Stylus.deltaY
 
     -- gestion de la sortie de l'image hors du cadre
-    if y > 192 - image_size then  -- s'il va trop bas on le régularise
-        y = 192 - image_size
+    if y > 192 -  Image.height(slime) then  -- s'il va trop bas on le régularise
+        y = 192 - Image.height(slime)
     end
     if y < 0 then -- s'il va trop haut on le régularise
         y = 0
     end
-    if  x > 256 - image_size then  -- s'il va trop à gauche on le régularise
-        x = 256 - image_size
+    if  x > 256 -  Image.width(slime) then  -- s'il va trop à gauche on le régularise
+        x = 256 - Image.width(slime)
     end
     if x < 0 then -- s'il va trop à droite on le régularise
         x = 0
