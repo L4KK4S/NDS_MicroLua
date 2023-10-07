e5c3a460e2f8a85419eaff7e922f736b1fbb68a7
Lakkas <abdel-waheb.sakkal@efrei.net>
2023-10-07 23:57:36 +0200

    add the collisions with borders and the scroll of the map

--- lua/scripts/map.lua
+++ lua/scripts/map.lua
@@ -1,12 +1,20 @@
+-- variables du jeu
+tile_size = 16
+map_x, map_y = 16, 15
+sprite_width, sprite_height = 24, 32
+x_velocity, y_velocity = 2, 2
+
 -- chargement des fichiers
 tiles = Image.load("data/img/tileset.png", VRAM) -- on charge les tiles
-map = ScrollMap.new(tiles, "map.map", 16, 12, 16, 16) -- on charge la map
-perso = Sprite.new("data/img/sprite.png", 24, 32, VRAM) -- on charge le sprite
+map = ScrollMap.new(tiles, "data/map/map.map", map_x, map_y, tile_size, tile_size) -- on charge la map
+perso = Sprite.new("data/img/sprite.png", sprite_width, sprite_height, VRAM) -- on charge le sprite
 
--- variables du joueurs
+-- variables du joueur
 direction = 4 -- direction du joueur - 1: bas - 2: gauche - 3: haut - 4: droite
 x, y = 0, 0 -- coordonnées du player
 pressed = false -- pression d'une touche
+x_scroll, y_scroll = 0, 0
+x_scroll_max, y_scroll_max = 0, 3 * tile_size
 
 -- Ajout des animations, avec un intervalle de 300ms entre chaque frame
 perso:addAnimation ({6,7,8}, 300)		-- Marcher vers le bas
@@ -20,18 +28,55 @@ while not Keys.newPress.Start do
     Controls.read()
     ScrollMap.draw(SCREEN_DOWN, map)
 
+
     touche = true  
 	if Keys.held.Down then -- si on presse la touche du bas alors on met à jour la direction et la position		
-		direction, y = 1, y+2	
+		direction = 1 -- met à jour la direction correspondante
+        if (y + sprite_height) < 192 then -- si on est pas à la bordure on se déplace
+            y = y + y_velocity
+        else
+            screen.print(SCREEN_UP, 0, 0, "DEPLACEMENT BAS")
+            if y_scroll < y_scroll_max then -- si on peut encore scroller la map on le fait
+                y_scroll = y_scroll + tile_size / 8
+            end
+            ScrollMap.scroll(map, x_scroll, y_scroll) -- on affiche le scroll de la map
+        end
 
 	elseif Keys.held.Left then -- si on presse la touche de gauche alors on met à jour la direction et la position	
-		direction, x = 2, x-2	
+		direction = 2
+        if x > 0 then
+            x = x - x_velocity
+        else
+            screen.print(SCREEN_UP, 0, 0, "DEPLACEMENT GAUCHE")
+            if x_scroll > 0 then -- si on peut encore scroller la map on le fait
+                x_scroll = x_scroll + tile_size / 8
+            end
+            ScrollMap.scroll(map, x_scroll, y_scroll) -- on affiche le scroll de la map
+        end
 
 	elseif Keys.held.Up then -- si on presse la touche du haut alors on met à jour la direction et la position 
-		direction, y = 3, y-2
+		direction = 3
+        if y > 0 then
+           y = y - y_velocity
+        else
+            screen.print(SCREEN_UP, 0, 0, "DEPLACEMENT HAUT")
+            if y_scroll > 0 then -- si on peut encore scroller la map on le fait
+                y_scroll = y_scroll - tile_size / 8
+            end
+            ScrollMap.scroll(map, x_scroll, y_scroll) -- on affiche le scroll de la map
+        end
 
 	elseif Keys.held.Right then -- si on presse la touche du droite  alors on met à jour la direction et la position 
-		direction, x = 4, x+2
+		direction = 4
+        if (x + sprite_width) < 256 then
+            x = x + x_velocity
+        else
+            screen.print(SCREEN_UP, 0, 0, "DEPLACEMENT DROITE")
+            if x_scroll < x_scroll then -- si on peut encore scroller la map on le fait
+                x_scroll = x_scroll + tile_size / 8
+            end
+            ScrollMap.scroll(map, x_scroll, y_scroll) -- on affiche le scroll de la map
+        end
 
 	else					
 		touche = false	
